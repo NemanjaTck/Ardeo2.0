35c642a2eaf4d674733e599b4a0c9c8f8bd202b7
Nemanja Drobnjak <nemanja.drobnjak91@gmail.com>
2018-09-29 22:03:17 +0200

    Portfolio custom post query

--- wp-content/themes/ardeo/index.php
+++ wp-content/themes/ardeo/index.php
@@ -1,3 +1,20 @@
-<?php get_header(); ?>
-<h1>Cao cao</h1>
-<?php get_footer(); ?>
+<?php get_header();?>
+
+<?php 
+  $portfolio_query = new WP_Query( array( 'post_type' => 'portfolio') ); ?>
+  
+<?php if ( $portfolio_query->have_posts() ) : ?>
+    <div class="portfolio-feed">
+        <?php while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post(); ?>
+            <article id="post-<?php the_ID() ?>" class="<?php post_class() ?>">
+                <h2><?php the_title(); ?></h2>
+            </article>
+        <?php endwhile; ?>
+
+        <?php wp_reset_postdata(); ?>
+    </div>
+<?php else : ?>
+	  <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
+
+<?php get_footer();?>
