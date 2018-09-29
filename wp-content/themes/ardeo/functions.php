62848a7e43615981fd0f4e80c4d73db3171af6d8
Nemanja Drobnjak <nemanja.drobnjak91@gmail.com>
2018-09-29 20:02:36 +0200

    Enqueue js script in functions.php

--- wp-content/themes/ardeo/functions.php
+++ wp-content/themes/ardeo/functions.php
@@ -2,6 +2,7 @@
 
 function ardeo_script_enqueue() {
   wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/ardeo.css', array(), '1.0.0', 'all');
+  wp_enqueue_script('customjs', get_template_directory_uri() . '/js/ardeo.js', array(), '1.0.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'ardeo_script_enqueue');
