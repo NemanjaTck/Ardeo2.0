f0b2f7a32ea6f109876ca1f84ed8605693557d93
Nemanja Drobnjak <nemanja.drobnjak91@gmail.com>
2018-09-30 00:12:16 +0200

    Semantic UI configuration and files

--- wp-content/themes/ardeo/functions.php
+++ wp-content/themes/ardeo/functions.php
@@ -1,6 +1,8 @@
 <?php
 
 function ardeo_script_enqueue() {
+  wp_enqueue_script( 'sui-script', get_stylesheet_directory_uri() . '/semantic/dist/semantic.min.js', array('jquery'), false, true );
+  wp_enqueue_style( 'sui-style', get_stylesheet_directory_uri() . '/semantic/dist/semantic.min.css' );
   wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/ardeo.css', array(), '1.0.0', 'all');
   wp_enqueue_script('customjs', get_template_directory_uri() . '/js/ardeo.js', array(), '1.0.0', true);
 }
--- wp-content/themes/ardeo/index.php
+++ wp-content/themes/ardeo/index.php
@@ -9,11 +9,18 @@
         <?php while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post(); ?>
             <article id="post-<?php the_ID() ?>" class="<?php post_class() ?>">
                 <h2><?php the_title(); ?></h2>
+                <p><?php the_content(); ?></p>
             </article>
         <?php endwhile; ?>
 
         <?php wp_reset_postdata(); ?>
     </div>
+    <button class="ui secondary button">
+      Edit
+    </button>
+    <button class="ui primary button">
+      Save
+    </button>
 <?php else : ?>
 	  <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
