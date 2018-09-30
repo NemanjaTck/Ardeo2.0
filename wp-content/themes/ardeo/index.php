8baba9f7d8617edbde2ac5cf57ab70c89d2123c9
Nemanja Drobnjak <nemanja.drobnjak91@gmail.com>
2018-09-30 13:54:40 +0200

    Portfolio flex, with teaser image as background

--- wp-content/themes/ardeo/index.php
+++ wp-content/themes/ardeo/index.php
@@ -5,22 +5,19 @@
   $portfolio_query = new WP_Query( array( 'post_type' => 'portfolio') ); ?>
 
 <?php if ( $portfolio_query->have_posts() ) : ?>
-    <div class="portfolio-feed">
+    <div class="portfolio__feed">
         <?php while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post(); ?>
+        <?php $teaser_field = get_field('teaser_image'); ?>
             <article id="post-<?php the_ID() ?>" class="<?php post_class() ?>">
-                <h2><?php the_title(); ?></h2>
-                <p><?php the_content(); ?></p>
+                <div class="portfolio__title">
+                  <h2><?php the_title(); ?></h2>
+                </div>
+                <img class="portfolio__background_image" src="<?php echo $teaser_field['url']; ?>" alt="<?php echo $teaser_field['alt']; ?>" />
             </article>
         <?php endwhile; ?>
 
         <?php wp_reset_postdata(); ?>
     </div>
-    <button class="ui secondary button">
-      Edit
-    </button>
-    <button class="ui primary button">
-      Save
-    </button>
 <?php else : ?>
 	  <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?>
