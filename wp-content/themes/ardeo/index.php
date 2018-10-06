e93d49b82c497748cd9aa498ee13e0b4a9d22433
Nemanja Drobnjak <nemanja.drobnjak91@gmail.com>
2018-10-06 21:40:18 +0200

    Adding Noto Sans font and contact section

--- wp-content/themes/ardeo/index.php
+++ wp-content/themes/ardeo/index.php
@@ -12,18 +12,29 @@ $portfolio_query = new WP_Query(array(
 )); ?>
 
 <?php if ($portfolio_query->have_posts()): ?>
-    <div class="portfolio__feed">
-        <?php while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post(); ?>
-            <?php $teaser_field = get_field('teaser_image'); ?>
-            <article id="post-<?php the_ID() ?>" class="<?php post_class() ?>">
-                <div class="portfolio__title">
-                    <h2><?php the_title(); ?></h2>
-                </div>
-                <img class="portfolio__background_image" src="<?php echo $teaser_field['url']; ?>" alt="<?php echo $teaser_field['alt']; ?>" />
-            </article>
-        <?php endwhile; ?>
+    <div id="section-portfolio">
+        <div class="portfolio__feed">
+            <?php while ( $portfolio_query->have_posts() ) : $portfolio_query->the_post(); ?>
+                <?php $teaser_field = get_field('teaser_image'); ?>
+                <article id="post-<?php the_ID() ?>" class="<?php post_class() ?>">
+                    <div class="portfolio__title">
+                        <h2><?php the_title(); ?></h2>
+                    </div>
+                    <img class="portfolio__background_image" src="<?php echo $teaser_field['url']; ?>" alt="<?php echo $teaser_field['alt']; ?>" />
+                </article>
+            <?php endwhile; ?>
+        </div>
     </div>
     <?php wp_reset_postdata(); ?>
 <?php endif; ?>
+
+<div id="section-contact">
+    <div class="contact__photo">
+        <?php echo wp_get_attachment_image( 21, Array('640', '480'), "", array( "class" => "img-responsive" ) ); ?> 
+    </div>
+    <div class="contact__form">
+        <?php echo do_shortcode( '[contact-form-7 id="20" title="Contact form"]' ); ?>  
+    </div>
+</div>
 
 <?php get_footer();?>
