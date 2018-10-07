38a545163045037065ac117b05a725cb6c2f6257
Nikola Milenković <1.nikola.milenkovic@gmail.com>
2018-10-07 23:25:54 +0200

    Style footer

--- wp-content/themes/ardeo/footer.php
+++ wp-content/themes/ardeo/footer.php
@@ -1,6 +1,23 @@
-    <footer>
-        Footer
-        <?php wp_nav_menu(array('theme_location'=>'secondary')); ?>
+    <footer class="footer">
+        <p class="footer__copy">Copyright &copy; <?php echo date('Y'); ?> Ardeo Solutions</p>
+
+        <ul class="footer__social">
+            <li class="footer__social-item">
+                <a href="" class="footer__social-link">
+                    <i class="facebook icon"></i>
+                </a>
+            </li>
+            <li class="footer__social-item">
+                <a href="" class="footer__social-link">
+                    <i class="instagram icon"></i>
+                </a>
+            </li>
+            <li class="footer__social-item">
+                <a href="" class="footer__social-link">
+                    <i class="linkedin icon"></i>
+                </a>
+            </li>
+        </ul>
     </footer>
 
     </div> <!-- Close pusher div -->
