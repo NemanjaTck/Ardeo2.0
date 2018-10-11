f6b67bbc3474109229693a3bda3f3eb2603df5cd
Nikola Milenković <1.nikola.milenkovic@gmail.com>
2018-10-11 22:24:53 +0200

    Add custom post type

--- wp-content/mu-plugins/custom-post-types.php
+++ wp-content/mu-plugins/custom-post-types.php
@@ -15,7 +15,24 @@ function custom_post_types () {
             'all_items' => 'All Projects',
             'singular_name' => 'Project'
         ),
-        'menu_icon' => 'dashicons-carrot'
+        'menu_icon' => 'dashicons-schedule'
+    ));
+
+    // Slideshow post type
+    register_post_type('slideshow', array(
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'rewrite' => array(
+            'slug' => 'slideshow'
+        ),
+        'public' => true,
+        'labels' => array(
+            'name' => 'Slideshow',
+            'add_new_item' => 'Add New Slide',
+            'edit_item' => 'Edit Slide',
+            'all_items' => 'All Slides',
+            'singular_name' => 'Slide'
+        ),
+        'menu_icon' => 'dashicons-camera'
     ));
 }
 
